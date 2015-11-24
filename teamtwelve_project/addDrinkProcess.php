81d8cef4002bd784172ea1b14350bc99e149e3e7
HiuFungC <14SIC012U@sit.singaporetech.edu.sg>
2015-11-24 13:13:38 +0800

    make changes to addIngredientProcess and addDrinkProcess

--- teamtwelve_project/addDrinkProcess.php
+++ teamtwelve_project/addDrinkProcess.php
@@ -1,17 +1,39 @@
-<?php
-session_start();
+<?php session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $drinkCategory= $_POST["inputDrinkCategory"];
-    $drinkType=$_POST["inputDrinkType"];
-    $drinkName=$_POST["inputDrinkName"];
-    $drinkPrice=$_POST["inputDrinkPrice"];
-
+{    
+    $mode;
+    if (isset($_POST["inputDrinkCategory"]))
+    {
+        $drinkCategory= $_POST["inputDrinkCategory"];
+        $drinkType=$_POST["inputDrinkType"];
+        $drinkName=$_POST["inputDrinkName"];
+        $drinkPrice=$_POST["inputDrinkPrice"];
+        $mode="insert";
+        echo $mode;
+    }
+    if (isset($_POST["updateDrinkCategory"]))
+    {
+        $updateDrinkCategory=$_POST["updateDrinkCategory"];
+        $updateDrinkType=$_POST["updateDrinkType"];
+        $updateDrinkName=$_POST["updateDrinkName"];
+        $updateDrinkPrice=$_POST["updateDrinkPrice"];
+        $updateImageLocation=$_POST["updateImageLocation"];
+        $updateDrinkId=$_POST["updateDrinkId"];
+        $mode="update";
+        echo $mode;
+    }
+    if (isset($_POST["deleteDrink"]))
+    {
+        $deleteDrink=$_POST["deleteDrink"];
+        echo $deleteDrink;
+        $mode="delete";
+        echo $mode;
+    }
 //    if ($usernameValid && $emailValid && $pwd1Valid && $pwd2Valid)
 //    {
 //        session_start();
         
-        require_once "../../protected/team12/config.php";
+        require_once("../../protected/team12/config.php");
         $host = DBHOST;
         $user = DBUSER;
         $pwd = DBPASS;
@@ -27,16 +49,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         }
         
         try{
-            $sql_insert = "INSERT INTO Drinkbase (DrinkCategory,DrinkType,DrinkName,price)
-                                   VALUES (?,?,?,?)";
+            if($mode=="insert")
+                {
+                $sql_insert = "INSERT INTO Drinkbase (DrinkCategory,DrinkType,DrinkName,price)
+                                       VALUES (?,?,?,?)";
 
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $drinkCategory);
-            $stmt->bindValue(2,  $drinkType);
-            $stmt->bindValue(3,  $drinkName);
-            $stmt->bindValue(4, $drinkPrice);
-            $stmt->execute();
-            header("Location: staffmenu.php");
+                $stmt = $conn->prepare($sql_insert);
+                $stmt->bindValue(1, $drinkCategory);
+                $stmt->bindValue(2,  $drinkType);
+                $stmt->bindValue(3,  $drinkName);
+                $stmt->bindValue(4, $drinkPrice);
+                $stmt->execute();
+                header("Location: staffmenu.php");
+                }
+            if($mode=="update")
+                {
+                $sql_select = "SELECT * FROM dbo.Drinkbasewhere DrinkId =$updateDrinkId" ;
+                $stmt = $conn->query($sql_select);
+                $drinks = $stmt->fetchAll(); 
+                $result=count($drinks);
+                if($result==1)
+                    {
+                    $sql_update = "UPDATE dbo.Drinkbase SET DrinkCategory='$updateDrinkCategory',DrinkType='$updateDrinkType',DrinkName='$updateDrinkName',price='$updateDrinkPrice' WHERE DrinkId =$updateDrinkId" ;
+                    $Query = $conn->query($sql_update);
+                    header("Location: staffmenu.php");
+                    }
+                 }
+            if($mode=="delete")
+                {
+                $sql_select = "SELECT * FROM dbo.Drinkbase where DrinkId =$deleteDrink" ;
+                $stmt = $conn->query($sql_select);
+                 $drinks  = $stmt->fetchAll(); 
+                $result=count($drinks);
+                if($result==1)
+                    {
+                    $sql_delete = "DELETE FROM dbo.Drinkbase where DrinkId =$deleteDrink" ;
+                    $Query = $conn->query($sql_delete);
+                    header("Location: staffmenu.php");
+                    }
+                 }
         }
 
 
